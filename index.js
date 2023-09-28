696377781a70efe28042a45a765af340240aece8
HiepTD <hiepqd02@gmail.com>
2023-09-28 23:43:19 +0700

    test

--- index.js
+++ index.js
@@ -83,58 +83,58 @@ app.get('/music', async (req, res) => {
   const { url, limit } = req.query // Use req.query to get query parameters
   const data = []
   try {
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    })
-    const page = await browser.newPage()
+    // const browser = await puppeteer.launch({
+    //   args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    // })
+    // const page = await browser.newPage()
 
-    await page.goto(url) // Replace with your URL
+    // await page.goto(url) // Replace with your URL
 
-    await page.keyboard.press('Escape')
+    // await page.keyboard.press('Escape')
 
-    let videoUrls = []
+    // let videoUrls = []
 
-    while (true) {
-      const newVideoUrls = await page.$$eval(
-        '[class*=AMetaCaptionLine]',
-        (elements) => {
-          return elements.map((element) => element.getAttribute('href'))
-        }
-      )
+    // while (true) {
+    //   const newVideoUrls = await page.$$eval(
+    //     '[class*=AMetaCaptionLine]',
+    //     (elements) => {
+    //       return elements.map((element) => element.getAttribute('href'))
+    //     }
+    //   )
 
-      videoUrls = videoUrls.concat(newVideoUrls)
+    //   videoUrls = videoUrls.concat(newVideoUrls)
 
-      if (limit && videoUrls.length >= limit) {
-        videoUrls = videoUrls.slice(0, limit)
-        break
-      }
+    //   if (limit && videoUrls.length >= limit) {
+    //     videoUrls = videoUrls.slice(0, limit)
+    //     break
+    //   }
 
-      const canScroll = await page.evaluate(() => {
-        const scrollableElement = document.documentElement
-        const scrollTop = scrollableElement.scrollTop
-        const scrollHeight = scrollableElement.scrollHeight
-        const clientHeight = scrollableElement.clientHeight
-        return scrollTop + clientHeight < scrollHeight
-      })
+    //   const canScroll = await page.evaluate(() => {
+    //     const scrollableElement = document.documentElement
+    //     const scrollTop = scrollableElement.scrollTop
+    //     const scrollHeight = scrollableElement.scrollHeight
+    //     const clientHeight = scrollableElement.clientHeight
+    //     return scrollTop + clientHeight < scrollHeight
+    //   })
 
-      if (canScroll) {
-        await page.evaluate(() => {
-          window.scrollBy(0, window.innerHeight)
-        })
-        await page.waitForTimeout(1000)
-      } else {
-        break // If you can't scroll anymore, exit the loop
-      }
-    }
-    await browser.close()
+    //   if (canScroll) {
+    //     await page.evaluate(() => {
+    //       window.scrollBy(0, window.innerHeight)
+    //     })
+    //     await page.waitForTimeout(1000)
+    //   } else {
+    //     break // If you can't scroll anymore, exit the loop
+    //   }
+    // }
+    // await browser.close()
 
-    for (const el of videoUrls) {
-      const response = await axios.get(
-        `https://developers.tiklydown.me/api/download?url=${el}`
-      )
-      data.push(response.data)
-    }
-    res.send(data)
+    // for (const el of videoUrls) {
+    //   const response = await axios.get(
+    //     `https://developers.tiklydown.me/api/download?url=${el}`
+    //   )
+    //   data.push(response.data)
+    // }
+    res.send('hiepvjpsieucappro')
   } catch (error) {
     console.error(error)
     res.status(500).send('Internal Server Error') // Handle errors gracefully and send an error response
